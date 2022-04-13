1d97dd2b03f576480678b0b6c3d0afb9a4e9e0f1
jsolis <jsolis@macroaplicaciones.com>
2022-04-12 20:28:07 -0800

    Mejorar implementación de AngularMaterial

--- uno_pf_solis/src/app/modules/material.module.ts
+++ uno_pf_solis/src/app/modules/material.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
+import { MatRadioModule } from '@angular/material/radio';
 
 @NgModule({
-  exports : [ MatInputModule, MatToolbarModule, MatIconModule, MatGridListModule, MatSidenavModule, MatButtonModule, MatTableModule ]
+  exports : [ MatInputModule, MatFormFieldModule, MatSelectModule, MatToolbarModule,
+    MatIconModule, MatGridListModule, MatSidenavModule, MatButtonModule, MatTableModule,
+    MatRadioModule ]
 })
 export class MaterialModule { }
